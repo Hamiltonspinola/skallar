a4a4ea273707bff4e05cf26fe4a52d59c7a6b6b5
Hamiltonspinola <hamiltonmoraes64@gmail.com>
2025-01-28 19:36:01 -0300

    feat: preenchendo migration do produto

--- database/migrations/2025_01_28_222804_create_products_table.php
+++ database/migrations/2025_01_28_222804_create_products_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->double('price', 8, 3);
+            $table->integer('quantity');
             $table->timestamps();
         });
     }
