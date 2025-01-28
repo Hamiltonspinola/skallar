5b2b22d491ade835e61e044a228c4464c3658004
Hamiltonspinola <hamiltonmoraes64@gmail.com>
2025-01-28 19:39:30 -0300

    feat: preenchendo factory e seed do produto

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -17,7 +17,10 @@ class ProductFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->words(3, true),
+            'description' => $this->faker->sentence(),
+            'price' => $this->faker->randomFloat(3, 1, 1000),
+            'quantity' => $this->faker->numberBetween(1, 100),
         ];
     }
 }
